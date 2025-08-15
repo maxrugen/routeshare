b70ea7e7212ecf59ccd544b6f87a57ebfb41594f
Max Rugen <max.rugen@staffbase.com>
2025-08-15 22:35:19 +0200

    changed port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.listen(PORT, () => {
